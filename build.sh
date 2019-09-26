2f75ff788c6b9a44fdf9e67c39f372d3cf1788b9
Gavin Bauer <gavinb@impro.net>
2019-09-26 12:33:38 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 sudo rm -f output/debs/linux-image*.deb
+sudo rm -f output/debs/linux-dtb*.deb
+sudo rm -f output/debs/linux-libc*.deb
+sudo rm -f output/debs/linux-headers*.deb
 sudo rm -rf output/patch
 sudo rm -rf output/images
 ./compile.sh
